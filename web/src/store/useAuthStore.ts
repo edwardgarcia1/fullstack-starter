5bd972b318e6a664bf02ef3860b6d0d67df3b3ba
Edward Garcia <edward.ariola.garcia@gmail.com>
2026-04-21 10:03:49 +0800

    fixed: go to dashboard every refresh

--- web/src/store/useAuthStore.ts
+++ web/src/store/useAuthStore.ts
@@ -19,7 +19,7 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>((set) => ({
 	user: null,
-	isLoading: false,
+	isLoading: true,
 	login: (user) => {
 		set({ user: user, isLoading: false });
 	},
