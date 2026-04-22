20d24cb47dd2a4cdd16bea6f33872dc7331d8e5d
Edward Garcia <edward.ariola.garcia@gmail.com>
2026-04-22 09:30:06 +0800

    added: swagger docs

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,13 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 import { authRoutes } from "./modules/users/auth.routes";
 import { userRoutes } from "./modules/users/users.routes";
 import { errorMiddleware } from "./middlewares/error";
 
 export const routes = new Elysia({ prefix: "/api" })
-	.use(errorMiddleware)
-	.use(authRoutes)
-	.use(userRoutes);
+    .use(swagger({
+        path: "/docs",
+    }))
+    .use(errorMiddleware)
+    .use(authRoutes)
+    .use(userRoutes);
